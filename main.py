bd7a95fd128ebd052f51503ff55c95bcbf5f0ca7
Chhunkea12 <kimchhunkea120@gmail.com>
2024-03-19 14:10:31 -0700

    Chhunkea12 leader team commit 1

---
+++ main.py
@@ -0,0 +1,10 @@
+name = 'Chhunkea'
+weight = 70
+height = 175
+
+print('Your name is:', name)
+print('Your height is:', height)
+print('Your weight is:', weight)
+
+BMI = weight / (height / 100) ** 2
+print('Sum up your name is:', name, 'and your BMI is :', BMI)
